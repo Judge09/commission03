6cf80a73b0318d4408d7d6b526ba1d82125d7467
Judge09 <133473201+Judge09@users.noreply.github.com>
2025-09-27 11:40:59 +0000

    test last

--- soul-good/src/supabaseClient.js
+++ soul-good/src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace these with your Supabase project credentials
-const supabaseUrl = "https://lmybaqaqfaswwhekdugs.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxteWJhcWFxZmFzd3doZWtkdWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODkxODAsImV4cCI6MjA3NDM2NTE4MH0.PvpeRwIqZv_hueZbJdF9UgsQONIJEUI2tnJ9SBB3xzs";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
